9d2b2071e7ceed6f0b32be33a601faf2eca36f40
Annonnymmousss <jatin02012006@gmail.com>
2025-07-27 19:19:48 +0530

    learning : struct

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -5,9 +5,22 @@ contract SimpleStorage {
     //by default variable declaration is internal
     // if u have to store smtg which is visible outside the contract u should make it public
     uint256 public FavouriteNumber; //0
+
+    uint256[] listofFavouriteNumber; //list
     function store(uint256 _FavouriteNumber)public { //every smart contract has its own unique address
         FavouriteNumber = _FavouriteNumber;
     }
+
+    struct Person{
+        uint256 number;
+        string name;
+    }
+
+
+    Person public pat = Person({
+        number : 7 ,
+        name : annonnymmousss
+    });
 
     //view,pure
     function retrieve() public view returns (uint256) {     //it can only return smtg which is to view only u cannot add changes or edit things in it 
