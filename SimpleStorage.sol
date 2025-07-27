93caed6338c2b41fb518f6c62efa8b14c55285fa
Annonnymmousss <jatin02012006@gmail.com>
2025-07-27 16:47:50 +0530

    learn : declaring variable as publiuc

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -2,8 +2,10 @@
 pragma solidity 0.8.19; //this is solidity version
 
 contract SimpleStorage {
-    uint256 FavouriteNumber;
-    function store(uint256 _FavouriteNumber)public {
+    //by default variable declaration is internal
+    // if u have to store smtg which is visible outside the contract u should make it public
+    uint256 public FavouriteNumber; //0
+    function store(uint256 _FavouriteNumber)public { //every smart contract has its own unique address
         FavouriteNumber = _FavouriteNumber;
     }
 }
